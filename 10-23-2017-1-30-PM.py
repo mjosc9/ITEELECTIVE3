86e55a686010e4e4c454155a8d2cb0a89e37fbec
mjosc9 <33021254+mjosc9@users.noreply.github.com>
2017-10-23 16:45:24 +0800

    Add files via upload

---
+++ 10-23-2017-1-30-PM.py
@@ -0,0 +1,11 @@
+rainfile = open("rainfall.txt","r")
+outfile = open("rainfallfmt.txt","w")
+
+for aline in rainfile:
+    values = aline.split()
+
+    city = values[0]
+    inches = float(values(1))
+    outfile.write("%+25s %5.1 f nn" %(city, inches))
+    rainfile.close()
+    outfile.close()
